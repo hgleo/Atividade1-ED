77d2625ea70e1da5a57f7f3eff1d048a60890f41
hgleo <hleomelo1@gmail.com>
2023-04-18 13:33:56 -0300

    commit

---
+++ Atvd1ED.c
@@ -0,0 +1,15 @@
+/*
+Enunciado:
+
+## Implemente um programa que apresente na tela o seguinte menu de opções
+
+1- Ler uma árvore de um arquivo fornecido pelo usuário
+2- Imprimir a árvore (opões: pré-ordem,em-ordem,pós-ordem)
+3- Verificar se um elemento x existe na árvor
+4- Contar o número de elementos na árvore
+5- Imprimir os nós folhas da árvore
+6- Verificar se uma árvore está balanceada
+7- Verificar se uma árvore é chei
+8- Imprimir o nível de um nó x
+9- Sair
+*/
