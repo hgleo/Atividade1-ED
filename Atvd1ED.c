ad4e91620dc69e7d16d11f47772fe00325fe288c
not-felipe <felipeam@id.uff.br>
2023-04-18 14:30:21 -0300

    menu

--- Atvd1ED.c
+++ Atvd1ED.c
@@ -24,7 +24,7 @@ typedef struct arvore{
 
 }arvore;
 
-int existeElemento(arvore *a){
+int existeElemento(arvore *a, int x){
 
   if(a ==NULL)
     return 0;
@@ -63,7 +63,7 @@ arvore *lerArvore(FILE *arq){
   }
 }
 
-void imprimePreOrdem(arvore* a,){
+void imprimePreOrdem(arvore* a){
 
   if(a != NULL){
     printf("%d", a->info);
@@ -123,12 +123,51 @@ void imprimeNivel(arvore *a,int cont, int nivel){
  } 
 }
 
+int menu(int option){
+  printf("Escolha uma das seguintes opcoes:\n[1] Ler uma arvore de um arquivo fornecido pelo usuário\n[2] Imprimir a arvore por pre-ordem/n[3] Imprimir a arvore em ordem\n[4]Imprimir a arvore por pos-ordem/n[5] Verificar se um elemento X existe na arvore\n[6] Contar o numero de elementos de uma arvore\n[7] Imprimir os nos das folhas da arvore\n[8] Verificar se uma arvore esta balanceada\n[9] Verificar se uma arvore e cheia\n[10] Imprimir o nivel de um no X\n [11] Sair");
+  do{
+  switch(option){
+    case 1:
+      lerArvore(a);
+      break;
+    case 2:
+      imprimePreOrdem(a);
+      break;
+    case 3:
+      imprimeEmOrdem(a);
+      break;
+    case 4:
+      imprimePosOrdem(a);
+      break;
+    case 5:
+      printf("Qual elemento deseja procurar?");
+      scanf("%d", &x);
+      existeElemento(a, x);
+      break;
+    case 6:
+      break;
+    case 7:
+      break;
+    case 8:
+      break;
+    case 9:
+      break;
+    case 10:
+      break;
+    case 11:
+      printf("Encerrando...");
+      break;
+  }
+  } while(option != 11);
+} 
+
 int main (){
 
   FILE *arq;
   arvore *a = NULL;
 
   arq = fopen("Entrada.txt", "r");
+  menu(option);
   a = lerArvore(arq);
   imprimeNivel(a,0,n);
   fclose(arq);
