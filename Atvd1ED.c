d0c5dd4ea15b153202d425eb02752a61572d39bf
hgleo <hleomelo1@gmail.com>
2023-04-24 10:49:56 -0300

    codigo lindo

--- Atvd1ED.c
+++ Atvd1ED.c
@@ -1,18 +1,12 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<string.h>
 
 typedef struct arvore{
-
     int info;
     struct arvore *esq;
     struct arvore *dir;
 }arvore;
-
 arvore *lerArvore(FILE *arq) {
-
-
-
     arvore *a = NULL;
     char c;
     int num;
@@ -33,36 +27,28 @@ arvore *lerArvore(FILE *arq) {
         return a;
     }
 }
-
 void imprimirPreOrdem(arvore *a){
-
     if(a != NULL){
       printf("%d ", a->info);
       imprimirPreOrdem(a->esq);
       imprimirPreOrdem(a->dir);  
     }
 }
-
 void imprimirPosOrdem(arvore *a){
-
     if(a != NULL){
       imprimirPosOrdem(a->esq);
       imprimirPosOrdem(a->dir);
       printf("%d ", a->info);
     }
 }
-
 void imprimirEmOrdem(arvore *a){
-
     if(a != NULL){
       imprimirEmOrdem(a->esq);
       printf("%d ", a->info);
       imprimirEmOrdem(a->dir);
     }
 }
-
 void imprimirNivel(arvore *a, int cont, int n){
-
   if(a != NULL){
     if(cont == n) 
       printf("%d ", a->info);
@@ -72,9 +58,7 @@ void imprimirNivel(arvore *a, int cont, int n){
     }
   }
 }
-
 int altura(arvore *a){
-
   if (a == NULL) return 0;
   else{
     int he = altura(a->esq);
@@ -83,17 +67,13 @@ int altura(arvore *a){
     else return hd + 1;
   }
 }
-
 void imprimirEmLargura(arvore *a){
-
   int h = altura(a);
   for(int i = 1; i <= h; i++){
     imprimirNivel(a, 1, i);
   }
 }
-
 int procurarElemento(arvore *a, int x){
-
     if(a == NULL){
         return 0;
     }
@@ -104,18 +84,14 @@ int procurarElemento(arvore *a, int x){
         return procurarElemento(a->esq, x) || procurarElemento(a->dir, x);
     }
 }
-
 int contarElementos(arvore *a){
-
   if(a == NULL){
     return 0;
   }
   else
     return 1 + contarElementos(a->esq) + contarElementos(a->dir);
 }
-
 void imprimirFolhas(arvore *a){
-
   if(a != NULL){
     if(a->dir == NULL && a->esq == NULL)
       printf("%d ", a->info);
@@ -124,9 +100,7 @@ void imprimirFolhas(arvore *a){
   imprimirFolhas(a->dir);
 }
 }
-
 int verificarArvoreBalanceada(arvore *a){
-
     if(a == NULL)
         return 1;
 
@@ -139,9 +113,7 @@ int verificarArvoreBalanceada(arvore *a){
 
     return 0;
 }
-
 int verificarArvoreCheia(arvore *a){
-
   if(a == NULL) return 1;
 
   if(a->esq == NULL && a->dir == NULL) return 1;
@@ -150,9 +122,7 @@ int verificarArvoreCheia(arvore *a){
 
   return 0;
 }
-
 int nivelDoNo(arvore *a, int x, int nivel){
-
   if(a == NULL) return -1; //menos 1, porque nesse caso o nivel pode ser 0
 
   if(a->info == x) return nivel;
@@ -164,25 +134,20 @@ int nivelDoNo(arvore *a, int x, int nivel){
 
   return nivel1;
 }
-
 void freeArvore(arvore *a) {
-
     if (a != NULL) {
         freeArvore(a->esq);
         freeArvore(a->dir);
         free(a);
     }
 }
-
 int main(){
-
 
     arvore *a = NULL;
     FILE *arq;
     int option, optionCase2;
 
   do{
-    system("clear");
     printf("[1] Ler uma arvore\n[2] Imprimir Arvore\n[3] Verificar Elemento\n[4] Contar numero de elementos da arvore\n[5] Imprimir nos folhas da arvore\n[6] Verificar se uma arvore esta balanceada\n[7] Verificar se uma arvore esta cheia\n[8] Imprimir o nivel de um no X\n[9] Sair\n");  
     scanf("%d", &option);
     switch(option){
