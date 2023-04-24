49fb5cbf8f811b0787f6792970fa8e8bda1904c3
hgleo <hleomelo1@gmail.com>
2023-04-24 10:28:53 -0300

    commit

--- Atvd1ED.c
+++ Atvd1ED.c
@@ -1,6 +1,6 @@
-#include<stdio.h>
-#include<stdlib.h>
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 typedef struct arvore{
     int info;
     struct arvore *esq;
@@ -73,85 +73,128 @@ int contarElementos(arvore *a){
     return 1 + contarElementos(a->esq) + contarElementos(a->dir);
 }
 
-  int main(){
+int altura(arvore *a){
 
+  if(a == NULL)
+    return 0;
+
+  else{
+
+    int he,hd;
+    he = altura(a->esq);
+    hd = altura(a->dir);
+
+    if(he>hd)
+      return he+1;
+    else
+      return hd+1;  
+  
+  }
+}
+
+void imprimeNivel(arvore *a,int cont, int nivel){
+
+ if(a != NULL){
+   if(cont == nivel){
+     printf("%d",a->info);     
+   }
+   else{
+     imprimeNivel(a->esq,cont+1,nivel); // subesq
+     imprimeNivel(a->dir,cont+1,nivel); // subdir
+   }
+ } 
+}
+
+int main() {
     arvore *a = NULL;
-    FILE *arq;
-    int option, optionCase2;
+    FILE *arq = NULL;
+    int option, optionCase2, x;
+    char nomeArq[50]; 
 
-  do{
-    printf("[1] Ler uma arvore\n[2] Imprimir Arvore\n[3] Verificar Elemento\n[4] Contar numero de elementos da arvore\n[5] Imprimir nos da arvore\n[6] Verificar se uma arvore esta balanceada\n[7] Verificar se uma arvore esta cheia\n[8] Imprimir o nivel de um no X\n[9] Sair\n");  
-    scanf("%d", &option);
-    switch(option){
-      case 1:{
-        char nomeArq[50]; 
-        printf("Digite o nome do arquivo que deseja ler: ");
-        scanf("%s", nomeArq);
-        arq = fopen(nomeArq, "r");
-        if(arq == NULL) {
-          printf("Erro ao abrir arquivo!\n");
-          break;
+    do {
+        printf("[1] Ler uma arvore\n[2] Imprimir Arvore\n[3] Verificar Elemento\n[4] Contar numero de elementos da arvore\n[5] Imprimir nos da arvore\n[6] Verificar se uma arvore esta balanceada\n[7] Verificar se uma arvore esta cheia\n[8] Imprimir o nivel de um no X\n[9] Sair\n");  
+        scanf("%d", &option);
+        switch(option) {
+            case 1:
+                
+                printf("Digite o nome do arquivo que deseja ler: ");
+                scanf("%s", nomeArq);
+                arq = fopen(nomeArq, "r");
+                if(arq == NULL) {
+                    printf("Erro ao abrir arquivo!\n");
+                    break;
+                }
+                a = lerArvore(arq);
+                fclose(arq);
+                break;
+            case 2:
+                if (a == NULL) {
+                    printf("Arvore nao carregada!\n");
+                    break;
+                }
+                printf("[1] Imprimir Pre-Ordem\n[2] Imprimir Em Ordem\n[3] Imprimir Pos Ordem\n[4] Imprimir em Largura\n");
+                scanf("%d", &optionCase2);
+                switch(optionCase2){
+                    case 1:
+                        imprimirPreOrdem(a);
+                        printf("\n");
+                        break;
+                    case 2:
+                        imprimirEmOrdem(a);
+                        printf("\n");
+                        break;
+                    case 3:
+                        imprimirPosOrdem(a);
+                        printf("\n");
+                        break;
+                    default:
+                        printf("Opcao Invalida!\n");
+                        break;
+                }
+                break;
+            case 3:
+                if (a == NULL) {
+                    printf("Arvore nao carregada!\n");
+                    break;
+                }
+                printf("Digite o elemento que deseja buscar na arvore: ");
+                scanf("%d", &x);
+                if(procurarElemento(a, x)){
+                    printf("Elemento %d encontrado!!\n", x);
+                } else {
+                    printf("Elemento %d nao encontrado na arvore!!\n", x);
+                }
+                break;
+            case 4:
+                if (a == NULL) {
+                    printf("Arvore nao carregada!\n");
+                    break;
+                }
+                printf("Numero de elementos da arvore: %d\n", contarElementos(a));
+                break;
+            case 5:
+                // implementar
+                break;
+            case 6:
+                // implementar
+                break;
+            case 7:
+                // implementar
+                break;
+            case 8:
+                // implementar
+                break;
+            case 9:
+                printf("Encerrando Programa...\n");
+                if (a != NULL) {
+                    free(a);
+                }
+                break;
+            default:
+                printf("Opcao Invalida!\n");
+                break;
         }
-        a = lerArvore(arq);
-        
-        break;
-      }
-      case 2:
-        printf("[1] Imprimir Pre-Ordem\n[2] Imprimir Em Ordem\n[3] Imprimir Pos Ordem\n[4] Imprimir em Largura\n");
-        scanf("%d", &optionCase2);
-        switch(optionCase2){
-          case 1:
-            imprimirPreOrdem(a);
-            printf("\n");
-            break;
-          case 2:
-            imprimirEmOrdem(a);
-            printf("\n");
-            break;
-          case 3:
-            imprimirPosOrdem(a);
-            printf("\n");
-            break;
-        }
-        break;
-      case 3:
-      {
-        int x;
-        printf("Digite o elemento que deseja buscar na arvore: ");
-        scanf("%d", &x);
-        if(procurarElemento(a, x)){
-          printf("Elemento %d encontrado!!\n", x);
-          break;
-        }
-        else
-          printf("Elemento %d nao encontrado na arvore!!\n", x);
-        break;
-      }
-      case 4:
-          {
-            int count = contarElementos(a);
-            printf("Numero de elementos da arvore: %d\n", count);
-          }
-        break;
-      case 5:
+    } while(option != 9);
 
-        break;
-      case 6:
-        break;
-      case 7:
-        break;
-      case 8:
-        break;
-      case 9:
-        printf("Encerrando Programa...\n");
-        fclose(arq);
-        break;
-      default:
-        printf("Opcao Invalida!\n");
-        break;
-    }
-    }while(option != 9);
-
-
-return 0;
+    return 0;
 }
